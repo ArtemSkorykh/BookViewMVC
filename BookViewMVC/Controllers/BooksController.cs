3cb63f59911b18c9ea8554228132a339a92a0367
Artem_Skorykh <artemskoryh8@gmail.com>
2024-08-12 23:36:52 +0200

    Add project files.

---
+++ BookViewMVC/Controllers/BooksController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BookViewMVC.Models;
+
+namespace BookViewMVC.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            var books = GetBooks();
+            return View(books);
+        }
+        public IActionResult Details(int id)
+        {
+            var books = GetBooks();
+            var book = books.FirstOrDefault(b => b.Id == id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        private List<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book
+                {
+                    Id = 1,
+                    Title = "1984",
+                    Author = "George Orwell",
+                    Genre  = "Dystopian Novel",
+                    Year = 1948
+                },
+                new Book
+                {
+                    Id = 2,
+                    Title = "The Adventure of the Blue Carbuncle",
+                    Author = "Arthur Conan Doyle",
+                    Genre = "Detective fiction",
+                    Year = 1892
+                },
+
+            };
+        }
+    }
+}
